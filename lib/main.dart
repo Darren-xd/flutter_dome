52ef26161c93ab8786c0684b48ed92a218062126
Darren xu <874111575@qq.com>
2019-06-22 17:31:28 +0800

    PageView组件使用

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,91 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.pink,
       ),
-      home: Text("Flutter Dome"),
+      home: RootView(),
     );
+  }
+}
+
+class RootView extends StatefulWidget {
+  @override
+  _RootViewState createState() => _RootViewState();
+}
+
+class _RootViewState extends State<RootView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+        leading: IconButton(
+          icon: Icon(Icons.account_circle),
+          onPressed: () {
+            print("Icon click");
+          },
+        ),
+      ),
+      body: HomePage(),
+      drawer: Drawer(
+        child: Text("Drawer"),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageViewDemo();
+  }
+}
+
+class PageViewDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return PageView(
+      controller: PageController(
+        initialPage: 0,
+        // keepPage: false,
+        // viewportFraction: 0.8,
+      ),
+      scrollDirection: Axis.vertical,
+      children: <Widget>[
+        Container(
+          alignment: Alignment.center,
+          color: Colors.green,
+          child: Text(
+            "ONE",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 32.0,
+            ),
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          color: Colors.grey[900],
+          child: Text(
+            "Tow",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 32.0,
+            ),
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          color: Colors.red[200],
+          child: Text(
+            "三",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 32.0,
+            ),
+          ),
+        ),
+      ],
+    );
+    ;
   }
 }
